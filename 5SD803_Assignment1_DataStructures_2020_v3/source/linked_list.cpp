5e08cce14c1fbf9731ebd70a5da072d8a6dde276
Dragonborn98760 <43043232+Dragonborn98760@users.noreply.github.com>
2020-11-10 10:02:57 +0100

    Monday Fenix

--- 5SD803_Assignment1_DataStructures_2020_v3/source/linked_list.cpp
+++ 5SD803_Assignment1_DataStructures_2020_v3/source/linked_list.cpp
@@ -27,6 +27,7 @@ linked_list::linked_list()
 linked_list::~linked_list()
 {
 	
+	clear();
 
 
 }
@@ -42,14 +43,14 @@ void linked_list::push_front(int value)
 	}
 	else
 	{
-		node* = head_;
+		node* headNode = head_;
 		head_->next_ = newNode;
 		head_ = newNode;
 	}
 }
 
 void linked_list::push_back(int value) 
-{
+{	
 	node* newNode = new node(value, nullptr);
 
 	if (tail_ == nullptr)
@@ -59,7 +60,7 @@ void linked_list::push_back(int value)
 	}
 	else 
 	{
-		node* = tail_;
+		node* tailNode = tail_;
 		tail_->next_ = newNode;
 		tail_ = newNode;
 		
@@ -69,23 +70,82 @@ void linked_list::push_back(int value)
 void linked_list::insert(int value, int index)
 {
 	
+	node* newNode = new node(value, nullptr);
+	
+	if (head_ == nullptr)
+	{
+		head_ = newNode;
+	}
+	if (tail_ == nullptr)
+		{
+		tail_ = newNode;
+		} 
+	else if(head_->next_ == nullptr)
+	{
+		head_->next_ = newNode;
+	}
+	else
+	{
+		node* currentNode = head_;
+		for (int i = 0; i < index; i++)
+		{
+			
+			if (currentNode->next_ == nullptr)
+			{
+				return;
+			}
+			else
+			{
+				currentNode = currentNode->next_;
+				
+			}
+			
+		}
+		newNode->next_ = currentNode;
+		currentNode->next_ = newNode;
+	}
 
+		
 	
 }
 
 void linked_list::pop_front()
 {
-
+	node* currentHead = head_;
+	head_ = head_->next_;
+	delete currentHead;
 }
 
 void linked_list::pop_back()
 {
-
+	node* currentTail = tail_;
+	tail_ = tail_->next_;
+	delete currentTail;
 }
 
 void linked_list::remove(int index) 
 {
-
+	node* currentNode = head_;
+	node* deleteNode;
+	
+		
+		
+			for (int i = 0; i < index; i++)
+			{
+				if (currentNode->next_ == nullptr)
+				{
+					return;
+				}
+				else
+				{
+					currentNode = currentNode->next_;
+				}
+			}
+			deleteNode = currentNode;
+			currentNode = currentNode->next_;
+			delete deleteNode;
+		
+		
 }
 
 void linked_list::clear() 
